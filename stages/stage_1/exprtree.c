d064ddc929249543ce32ea0bef146c54e088582c
Alen Antony <alenantonyme@gmail.com>
2023-01-12 10:44:54 +0530

    Stage 1, started

---
+++ stages/stage_1/exprtree.c
@@ -0,0 +1,72 @@
+struct tnode* makeLeafNode(int n)
+{
+    struct tnode *temp;
+    temp = (struct tnode*)malloc(sizeof(struct tnode));
+    temp->op = NULL;
+    temp->val = n;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+struct tnode* makeOperatorNode(char c,struct tnode *l,struct tnode *r){
+    struct tnode *temp;
+    temp = (struct tnode*)malloc(sizeof(struct tnode));
+    temp->op = malloc(sizeof(char));
+    *(temp->op) = c;
+    temp->left = l;
+    temp->right = r;
+    return temp;
+}
+
+int evaluate(struct tnode *t){
+    if(t->op == NULL)
+    {
+        return t->val;
+    }
+    else{
+        switch(*(t->op)){
+            case '+' : return evaluate(t->left) + evaluate(t->right);
+                       break;
+            case '-' : return evaluate(t->left) - evaluate(t->right);
+                       break;
+            case '*' : return evaluate(t->left) * evaluate(t->right);
+                       break;
+            case '/' : return evaluate(t->left) / evaluate(t->right);
+                       break;
+        }
+    }
+}
+
+void prefix(struct tnode *t){
+
+    if(t==NULL)
+        return;
+    if(t->op)
+        printf("%c ",*(t->op));
+    else
+        printf("%d ",t->val);
+
+
+    prefix(t->left);
+    prefix(t->right);
+
+
+}
+void postfix(struct tnode *t){
+
+    if(t==NULL)
+        return;
+    
+    postfix(t->left);
+    postfix(t->right);
+    
+    if(t->op)
+        printf("%c ",*(t->op));
+    else
+        printf("%d ",t->val);
+
+
+
+
+}
